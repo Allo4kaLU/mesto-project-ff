c9dd8af2718bc8bf9ac91d2540033e9f7697067c
Alla Lutsenko <lucenko.allo4ka@yandex.ru>
2024-02-16 09:49:57 +0300

    Добавлены карточки с удалением

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Allo4kaLU/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-
+function createCard(link, name, deleteCard) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__title").textContent = name;
+  placesList.append(cardElement);
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(evt) {
+  evt.target.closest(".places__item").remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (element) {
+  createCard(element.link, element.name, deleteCard);
+});
