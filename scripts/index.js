fce1558e5542d2bd3ec5703b818c4513d1343dcc
Alla Lutsenko <lucenko.allo4ka@yandex.ru>
2024-02-16 10:42:22 +0300

    Добавлено alt и return

--- scripts/index.js
+++ scripts/index.js
@@ -6,11 +6,13 @@ const placesList = document.querySelector(".places__list");
 function createCard(link, name, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__image").alt = name;
   cardElement.querySelector(".card__title").textContent = name;
   placesList.append(cardElement);
   cardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", deleteCard);
+  return cardElement;
 }
 // @todo: Функция удаления карточки
 function deleteCard(evt) {
