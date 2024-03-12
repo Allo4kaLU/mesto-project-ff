490fae7223c1036b2a56db8cac14ef5252c2230b
Alla Lutsenko <lucenko.allo4ka@yandex.ru>
2024-03-12 17:17:08 +0300

    Модальные два окна

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import {initialCards} from './scripts/cards';
 const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
 const placesList = document.querySelector(".places__list");
+
+
+const popupEdit = document.querySelector(".popup_type_edit");
+const editButton = document.querySelector(".profile__edit-button");
+const popupClose = document.querySelector(".popup__close");
+
+const popupCard = document.querySelector(".popup_type_new-card");
+const cardButton = document.querySelector(".profile__add-button");
+const poputCardClose = document.querySelector(".popup__card__close");
+
+const popupImage = document.querySelector(".popup_type_image");
+
+
 // @todo: Функция создания карточки
 function createCard(link, name, deleteCard) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
@@ -23,3 +36,75 @@ function deleteCard(evt) {
 initialCards.forEach(function (element) {
   placesList.append(createCard(element.link, element.name, deleteCard));
 });
+
+
+
+function closeOnBackDropClickPopupEdit({ currentTarget, target }) {
+  const popupEdit = currentTarget;
+  const isClickedOnBackDrop = target === popupEdit;
+  if (isClickedOnBackDrop) {
+    removePopupEdit();
+  }
+}
+
+window.onkeydown = function( event ) {
+  if ( event.keyCode == 27 ) {
+      removePopupEdit();
+  }
+};
+
+function openPopupEdit() {
+  popupEdit.classList.add('popup_is-opened');
+}
+
+function removePopupEdit() {
+  popupEdit.classList.remove('popup_is-opened');
+  popupEdit.classList.add('popup_is-animated');
+}
+
+popupEdit.addEventListener('click', closeOnBackDropClickPopupEdit);
+editButton.addEventListener('click', openPopupEdit);
+popupClose.addEventListener('click', removePopupEdit);
+
+
+
+function closeOnBackDropClickPopupCard({ currentTarget, target }) {
+  const popupCard = currentTarget;
+  const isClickedOnBackDrop = target === popupCard;
+  if (isClickedOnBackDrop) {
+    removePopupCard();
+  }
+}
+
+window.onkeydown = function( event ) {
+  if ( event.keyCode == 27 ) {
+    removePopupCard();
+  }
+};
+
+function openPopupCard() {
+  popupCard.classList.add('popup_is-opened');
+}
+
+function removePopupCard() {
+  popupCard.classList.remove('popup_is-opened');
+  popupCard.classList.add('popup_is-animated');
+}
+
+popupCard.addEventListener('click', closeOnBackDropClickPopupCard);
+cardButton.addEventListener('click', openPopupCard);
+poputCardClose.addEventListener('click', removePopupCard);
+
+
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_description');
+
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  jobInput.textContent = jobInput.value;
+  nameInput.textContent = nameInput.value;   
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
