616a2f36b786043566a89601e8d7bd0d1acd357f
Alla Lutsenko <lucenko.allo4ka@yandex.ru>
2024-03-29 15:54:51 +0300

    Валидация сделана

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 https://github.com/Allo4kaLU/mesto-project-ff.git
+https://allo4kalu.github.io/mesto-project-ff/
--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ function closeOnBackDropClickPopupEdit({ currentTarget, target }) {
 
 function openPopupEdit() {
   openModal(popupEdit);
+  clearValidation(popupEdit);
 }
 
 function removePopupEdit() {
@@ -137,7 +138,94 @@ function handleCardFormSubmit(evt) {
     )
   );
   formNewPlace.reset();
+  clearValidation();
   removePopupCard();
 }
 
 formNewPlace.addEventListener("submit", handleCardFormSubmit);
+
+// валидация форм
+const showInputError = (formSelector, inputSelector, errorMessage) => {
+  const errorElement = formSelector.querySelector(`.${inputSelector.id}-error`);
+  inputSelector.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input_type_error_active');
+};
+
+const hideInputError = (formSelector, inputSelector) => {
+  const errorElement = formSelector.querySelector(`.${inputSelector.id}-error`);
+  inputSelector.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input_type_error_active');
+  errorElement.textContent = '';
+};
+
+// Функция, которая проверяет валидность поля
+const enableValidationUR = (formSelector, inputSelector) => {
+  if (inputSelector.validity.patternMismatch) {
+    inputSelector.setCustomValidity(inputSelector.dataset.errorMessage);
+  } else {
+    inputSelector.setCustomValidity("");
+  } 
+  if (!inputSelector.validity.valid) {
+    // Если поле не проходит валидацию, покажем ошибку
+    showInputError(formSelector, inputSelector, inputSelector.validationMessage);
+  } else {
+    // Если проходит, скроем
+    hideInputError(formSelector, inputSelector);
+  }
+};
+
+function setEventListeners (formSelector) {
+  const inputList = Array.from(formSelector.querySelectorAll('.popup__input'));
+  const submitButtonSelector = formSelector.querySelector('.popup__button');
+  toggleButtonState(inputList, submitButtonSelector);
+  inputList.forEach((inputSelector) => {
+    inputSelector.addEventListener('input', function () {
+      enableValidationUR(formSelector, inputSelector);
+      toggleButtonState(inputList, submitButtonSelector);
+    });
+  });
+};
+
+function enableValidation () {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formSelector) => {
+    formSelector.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formSelector);
+  });
+};
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}); 
+
+// Функция переключения кнопки
+
+function hasInvalidInput (inputList) {
+  return inputList.some((inputSelector) => {
+    return !inputSelector.validity.valid;
+  })
+}; 
+
+function toggleButtonState (inputList, submitButtonSelector) {
+  if (hasInvalidInput(inputList)) {
+    submitButtonSelector.disabled = true;
+    submitButtonSelector.classList.add('popup__button_disabled');
+  } else {
+    submitButtonSelector.disabled = false;
+    submitButtonSelector.classList.remove('popup__button_disabled');
+  }
+};
+
+function clearValidation (formSelector) {
+  const errorElement = formSelector.querySelector('.popup__error_visible');
+  formSelector.querySelector('.popup__input').classList.remove('popup__input_type_error');
+  errorElement.textContent = '';
+}
